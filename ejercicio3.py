575cdddd205b0d3fcc9525a1ed9510a6558c04b3
MarcoYahmirAntunaLeyva <marco_3141230001@utd.edu.mx>
2024-05-08 23:20:25 +0200

    Revert "ejercicio1.py"
    This reverts commit 281388140f5388661d6a1014c098589d0832c666.

--- ejercicio3.py
+++
@@ -1,6 +0,0 @@
-base = float(input("Ingresa la base"))
-altura = float(input("Ingresa la altura"))
-
-area = base * altura
-
-print("El area es: ", area)
--- ejercicio4.py
+++
@@ -1,9 +0,0 @@
-import math 
-
-diamtero = float(input("Ingresa el diametro"))
-
-radio = diamtero/2
-
-area = math.pi * (radio*radio)
-
-print("El area es: ", area)
